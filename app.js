2e68b69b5467ddce980143c2b884eeb61ddc1838
tod619 <trevorodwyer619@hotmail.com>
2020-03-07 13:31:00 +0000

    add variables that are for use in the project

---
+++ app.js
@@ -0,0 +1,10 @@
+// Variables
+const currencyEL_one = document.getElementById("currency-one");
+const amountEl_one = document.getElementById("amount-one");
+const currencyEL_two = document.getElementById("currency-two");
+const amountEl_two = document.getElementById("amount-two");
+
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+console.log(amountEl_one.value);
