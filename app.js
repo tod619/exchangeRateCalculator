5a2b15f12f77c5e091beaa22b0eb79ceb6cf6a13
tod619 <trevorodwyer619@hotmail.com>
2020-03-08 16:27:25 +0000

    add the calculate function to app.js

--- app.js
+++ app.js
@@ -9,7 +9,18 @@ const swap = document.getElementById("swap");
 
 // fetch Exchange Rates and Update DOM
 function calculate() {
-  console.log("RAN");
+  const currency_one = currencyEL_one.value;
+  const currency_two = currencyEL_two.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+      //console.log(data);
+      const rate = data.rates[currency_two];
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
 }
 
 // Add EventListners
