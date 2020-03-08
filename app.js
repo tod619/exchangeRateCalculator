ff9427dcc4a43cc275effa93171dbdf2e6e76490
tod619 <trevorodwyer619@hotmail.com>
2020-03-08 16:03:39 +0000

    Add eventlistners to inputs and check if working with dummy function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Variables
-const currencyEL_one = document.getElementById("currency-one");
+const currencyEL_one = document.getElementById("currencey-one");
 const amountEl_one = document.getElementById("amount-one");
 const currencyEL_two = document.getElementById("currency-two");
 const amountEl_two = document.getElementById("amount-two");
@@ -7,4 +7,15 @@ const amountEl_two = document.getElementById("amount-two");
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
-console.log(amountEl_one.value);
+// fetch Exchange Rates and Update DOM
+function calculate() {
+  console.log("RAN");
+}
+
+// Add EventListners
+currencyEL_one.addEventListener("change", calculate);
+amountEl_one.addEventListener("input", calculate);
+currencyEL_two.addEventListener("change", calculate);
+amountEl_two.addEventListener("input", calculate);
+
+calculate();
